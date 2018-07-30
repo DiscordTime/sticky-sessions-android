2fa57faca8ea70dae44022bcf9b999231cc11a80
Eduardo Maia <ehammo@cin.ufpe.br>
2018-07-30 18:40:38 -0300

    Issue #17: Create Firebase Session Model
    Also changed google() to a maven equivalent,
    because of circleci gradle version
    
    Also changed config.yml to chmod ./gradlew
    
    It fixes #17

---
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/data/remote/model/Note.java
@@ -0,0 +1,23 @@
+package br.org.cesar.discordtime.stickysessions.data.remote.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Note {
+
+    @SerializedName("description")
+    @Expose
+    public String description;
+    @SerializedName("user")
+    @Expose
+    public String user;
+    @SerializedName("topic")
+    @Expose
+    public String topic;
+    @SerializedName("session_id")
+    @Expose
+    public String sessionId;
+
+    public Note(){}
+
+}
