2c9fc568ec40e12a08e865659df63dbba8da86a3
Eduardo maia <ehammo@motorola.com>
2018-07-31 14:47:09 -0300

    Create Domain Session Model
    It fixes #22

---
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/domain/model/Note.java
@@ -0,0 +1,10 @@
+package br.org.cesar.discordtime.stickysessions.domain.model;
+
+public class Note {
+
+    public String description;
+    public String user;
+    public String topic;
+    public String session_id;
+
+}
---
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/domain/model/Session.java
@@ -0,0 +1,9 @@
+package br.org.cesar.discordtime.stickysessions.domain.model;
+
+import java.util.List;
+
+class Session {
+    public String sessionId;
+    public List<String> topics = null;
+
+}
