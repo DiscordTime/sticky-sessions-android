708991563f6a4c467205c9d40badad75aaa1c045
Rodrigo Perazzo <rpr@cesar.org.br>
2018-09-20 10:41:15 -0300

    Enable prefetch and fix view start size/position on Lobby
    Enable prefetch of items on LayoutManager used by Lobby's
    RecyclerView to improve scroll animation rendering and fix
    start size and position of views to match PagerSnapHelper
    behavior.
    
    Fix #53.

--- app/src/main/java/br/org/cesar/discordtime/stickysessions/ui/lobby/SessionTypeLayoutManager.java
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/ui/lobby/SessionTypeLayoutManager.java
@@ -3,13 +3,23 @@ package br.org.cesar.discordtime.stickysessions.ui.lobby;
 import android.content.Context;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.OrientationHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class SessionTypeLayoutManager extends LinearLayoutManager {
 
     private final float mShrinkAmount = 0.15f;
     private final float mShrinkDistance = 0.9f;
+    private RecyclerView.Recycler mRecycler;
+
+    // Orientation helpers are lazily created per LayoutManager.
+    @Nullable
+    private OrientationHelper mVerticalHelper;
+    @Nullable
+    private OrientationHelper mHorizontalHelper;
 
     public SessionTypeLayoutManager(Context context) {
         super(context);
@@ -17,6 +27,33 @@ public class SessionTypeLayoutManager extends LinearLayoutManager {
 
     SessionTypeLayoutManager(Context context, int orientation, boolean reverseLayout) {
         super(context, orientation, reverseLayout);
+        setItemPrefetchEnabled(true);
+        setInitialPrefetchItemCount(1);
+    }
+
+    @Override
+    public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state, int widthSpec, int heightSpec) {
+        super.onMeasure(recycler, state, widthSpec, heightSpec);
+        mRecycler = recycler;
+        int orientation = getOrientation();
+        if (orientation == HORIZONTAL) {
+            shrinkHorizontally();
+        } else {
+            shrinkVertically();
+        }
+    }
+
+    @Override
+    public void onLayoutCompleted(RecyclerView.State state) {
+        super.onLayoutCompleted(state);
+        int orientation = getOrientation();
+        OrientationHelper helper;
+        if (orientation == HORIZONTAL) {
+            helper = getHorizontalHelper();
+        } else {
+            helper = getVerticalHelper();
+        }
+        snapToCenter(state, helper, orientation);
     }
 
     @Override
@@ -25,20 +62,7 @@ public class SessionTypeLayoutManager extends LinearLayoutManager {
         int orientation = getOrientation();
         if (orientation == VERTICAL) {
             int scrolled = super.scrollVerticallyBy(dy, recycler, state);
-            float midpoint = getHeight() / 2.f;
-            float d0 = 0.f;
-            float d1 = mShrinkDistance * midpoint;
-            float s0 = 1.f;
-            float s1 = 1.f - mShrinkAmount;
-            for (int i = 0; i < getChildCount(); i++) {
-                View child = getChildAt(i);
-                float childMidpoint =
-                        (getDecoratedBottom(child) + getDecoratedTop(child)) / 2.f;
-                float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
-                float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
-                child.setScaleX(scale);
-                child.setScaleY(scale);
-            }
+            shrinkVertically();
             return scrolled;
         } else {
             return 0;
@@ -51,25 +75,80 @@ public class SessionTypeLayoutManager extends LinearLayoutManager {
         int orientation = getOrientation();
         if (orientation == HORIZONTAL) {
             int scrolled = super.scrollHorizontallyBy(dx, recycler, state);
-
-            float midpoint = getWidth() / 2.f;
-            float d0 = 0.f;
-            float d1 = mShrinkDistance * midpoint;
-            float s0 = 1.f;
-            float s1 = 1.f - mShrinkAmount;
-            for (int i = 0; i < getChildCount(); i++) {
-                View child = getChildAt(i);
-                float childMidpoint =
-                        (getDecoratedRight(child) + getDecoratedLeft(child)) / 2.f;
-                float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
-                float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
-                child.setScaleX(scale);
-                child.setScaleY(scale);
-            }
+            shrinkHorizontally();
             return scrolled;
         } else {
             return 0;
         }
+    }
 
+    private void snapToCenter(RecyclerView.State state, OrientationHelper helper, int orientation) {
+        View child = getChildAt(0);
+        if (child != null) {
+            final int childCenter = helper.getDecoratedStart(child)
+                    + (helper.getDecoratedMeasurement(child) / 2);
+            final int containerCenter;
+            if (getClipToPadding()) {
+                containerCenter = helper.getStartAfterPadding() + helper.getTotalSpace() / 2;
+            } else {
+                containerCenter = helper.getEnd() / 2;
+            }
+
+            if (orientation == HORIZONTAL) {
+                scrollHorizontallyBy(childCenter - containerCenter, mRecycler, state);
+            } else {
+                scrollVerticallyBy(childCenter - containerCenter, mRecycler, state);
+            }
+        }
+    }
+
+    @NonNull
+    private OrientationHelper getVerticalHelper() {
+        if (mVerticalHelper == null) {
+            mVerticalHelper = OrientationHelper.createVerticalHelper(this);
+        }
+        return mVerticalHelper;
+    }
+
+    @NonNull
+    private OrientationHelper getHorizontalHelper() {
+        if (mHorizontalHelper == null) {
+            mHorizontalHelper = OrientationHelper.createHorizontalHelper(this);
+        }
+        return mHorizontalHelper;
+    }
+
+    private void shrinkVertically() {
+        float midpoint = getHeight() / 2.f;
+        float d0 = 0.f;
+        float d1 = mShrinkDistance * midpoint;
+        float s0 = 1.f;
+        float s1 = 1.f - mShrinkAmount;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            float childMidpoint =
+                    (getDecoratedBottom(child) + getDecoratedTop(child)) / 2.f;
+            float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
+            float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
+            child.setScaleX(scale);
+            child.setScaleY(scale);
+        }
+    }
+
+    private void shrinkHorizontally() {
+        float midpoint = getWidth() / 2.f;
+        float d0 = 0.f;
+        float d1 = mShrinkDistance * midpoint;
+        float s0 = 1.f;
+        float s1 = 1.f - mShrinkAmount;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            float childMidpoint =
+                    (getDecoratedRight(child) + getDecoratedLeft(child)) / 2.f;
+            float d = Math.min(d1, Math.abs(midpoint - childMidpoint));
+            float scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0);
+            child.setScaleX(scale);
+            child.setScaleY(scale);
+        }
     }
 }
