0bf73fa1c03a6cb774c16532e19300a673b7b9e4
AmaroNeto <amarovn@gmail.com>
2018-11-28 09:50:14 -0300

    Fix #127: Create List Session Test Cases
    Validation that are made:
    
    List Sessions Presenter
    - should init observers when present start onLoad
    - should dispose all observers after onPause
    - should start loading data when present start load
    - should stop loading view when listSession returns onSucess
    - should show sessions when listSession returns onSucess
    - should stop loading when listSession returns onError
    - should show error when listSession returns onError
    - should call goNext when enterSession receive a valid session
    - should showError when enterSession receive a invalid session
    
    List Sessions
    - should return all sessions avaliable

--- app/src/test/java/br/org/cesar/discordtime/stickysessions/domain/interactor/ListSessionsTest.kt
+++ app/src/test/java/br/org/cesar/discordtime/stickysessions/domain/interactor/ListSessionsTest.kt
@@ -1,8 +1,16 @@
 package br.org.cesar.discordtime.stickysessions.domain.interactor
 
+import br.org.cesar.discordtime.stickysessions.domain.model.Session;
 import br.org.cesar.discordtime.stickysessions.domain.repository.SessionRepository
+import br.org.cesar.discordtime.stickysessions.factory.DataFactory
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Single
+import io.reactivex.observers.TestObserver
 import org.junit.Before
+import org.junit.Test
+import java.util.*
 
 class ListSessionsTest {
 
@@ -11,6 +19,28 @@ class ListSessionsTest {
     @Before
     fun setUp() {
         mSessionRepositoryMock = mock()
+    }
+
+    @Test
+    fun `should return all sessions avaliable`() {
+        whenever(mSessionRepositoryMock.listSessions())
+                .thenReturn(Single.create {
+                    emitter ->
+                    val session = Session()
+                    session.id = DataFactory.randomString()
+                    session.topics = Arrays.asList(DataFactory.randomString())
+                    session.createdAt = DataFactory.randomString()
+
+                    emitter.onSuccess(Arrays.asList(session))})
+
+        val listSessions = ListSessions(mSessionRepositoryMock)
+        val singleListSessions : Single<List<Session>> = listSessions.execute(null)
+
+        val testObserver: TestObserver<List<Session>> = singleListSessions.test()
+        testObserver.awaitTerminalEvent()
+        testObserver.assertNoErrors()
+
+        verify(mSessionRepositoryMock).listSessions()
     }
 
 }
--- app/src/test/java/br/org/cesar/discordtime/stickysessions/presentation/list/ListSessionsPresenterTest.kt
+++ app/src/test/java/br/org/cesar/discordtime/stickysessions/presentation/list/ListSessionsPresenterTest.kt
@@ -2,14 +2,19 @@ package br.org.cesar.discordtime.stickysessions.presentation.list
 
 import br.org.cesar.discordtime.stickysessions.domain.model.Session
 import br.org.cesar.discordtime.stickysessions.executor.IObservableUseCase
+import br.org.cesar.discordtime.stickysessions.factory.DataFactory
 import br.org.cesar.discordtime.stickysessions.logger.Logger
 import br.org.cesar.discordtime.stickysessions.navigation.router.IRouter
+import br.org.cesar.discordtime.stickysessions.navigation.router.Route
+import br.org.cesar.discordtime.stickysessions.navigation.wrapper.BundleWrapper
+import br.org.cesar.discordtime.stickysessions.navigation.wrapper.IBundle
 import br.org.cesar.discordtime.stickysessions.navigation.wrapper.IBundleFactory
-import com.nhaarman.mockito_kotlin.KArgumentCaptor
-import com.nhaarman.mockito_kotlin.argumentCaptor
-import com.nhaarman.mockito_kotlin.mock
+import br.org.cesar.discordtime.stickysessions.ui.ViewNames
+import com.nhaarman.mockito_kotlin.*
 import io.reactivex.observers.DisposableSingleObserver
+import junit.framework.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 
 class ListSessionsPresenterTest {
     private lateinit var listSessionsPresenter : ListSessionsPresenter
@@ -19,6 +24,13 @@ class ListSessionsPresenterTest {
     private lateinit var mockIRouter: IRouter
     private lateinit var mockView: ListSessionsContract.View
     private lateinit var captor: KArgumentCaptor<DisposableSingleObserver<List<Session>>>
+    private lateinit var mListSession : List<Session>
+    private lateinit var mIbundle : IBundle
+    val validRoute = Route(
+            ViewNames.LOBBY_ACTIVITY,
+            IRouter.CREATED_SESSION,
+            ViewNames.SESSION_ACTIVITY,
+            false)
 
     @Before
     fun setUp() {
@@ -27,10 +39,101 @@ class ListSessionsPresenterTest {
         mockLogger = mock()
         mockIBundleFactory = mock()
         mockView = mock()
+        mIbundle = mock()
         listSessionsPresenter = ListSessionsPresenter(mockLisSessions, mockIRouter,
                 mockLogger, mockIBundleFactory)
 
         captor = argumentCaptor<DisposableSingleObserver<List<Session>>>()
+
+        mListSession = listOf<Session>(
+                Session()
+        )
+    }
+
+    @Test
+    fun `should init observers when present start onLoad`() {
+        listSessionsPresenter.attachView(mockView)
+        listSessionsPresenter.onLoad()
+        verify(mockLisSessions).execute(any(), isNull())
+    }
+
+    @Test
+    fun `should dispose all observers after onPause`() {
+        listSessionsPresenter.attachView(mockView)
+
+        listSessionsPresenter.onLoad()
+        verify(mockLisSessions).execute(captor.capture(), isNull())
+        listSessionsPresenter.onPause()
+        assertTrue(captor.firstValue.isDisposed)
+    }
+
+    @Test
+    fun `should start loading data when present start load`() {
+        listSessionsPresenter.attachView(mockView)
+        listSessionsPresenter.onLoad()
+        verify(mockView).startLoadingData()
+    }
+
+    @Test
+    fun `should stop loading view when listSession returns onSucess`() {
+        listSessionsPresenter.attachView(mockView)
+        configureListSessionSuccess()
+        verify(mockView).stopLoadingData()
+    }
+
+    @Test
+    fun `should show sessions when listSession returns onSucess`() {
+        listSessionsPresenter.attachView(mockView)
+        configureListSessionSuccess()
+        verify(mockView).showSessions(mListSession)
+    }
+
+    @Test
+    fun `should stop loading when listSession returns onError`() {
+        listSessionsPresenter.attachView(mockView)
+        configureListSessionError()
+        verify(mockView).stopLoadingData()
+    }
+
+    @Test
+    fun `should show error when listSession returns onError`() {
+        listSessionsPresenter.attachView(mockView)
+        configureListSessionError()
+        verify(mockView).showError(any())
+    }
+
+    @Test
+    fun `should call goNext when enterSession receive a valid session`() {
+        configureBundle()
+        listSessionsPresenter.attachView(mockView)
+        listSessionsPresenter.enterOnSession(mListSession.first())
+        verify(mockView).goNext(any(),any())
+    }
+
+    @Test
+    fun `should showError when enterSession receive a invalid session`() {
+        configureBundle()
+        listSessionsPresenter.attachView(mockView)
+        listSessionsPresenter.enterOnSession(null)
+        verify(mockView).showError(any())
+    }
+
+    private fun configureListSessionSuccess() {
+        listSessionsPresenter.onLoad()
+        verify(mockLisSessions).execute(captor.capture(), isNull())
+        captor.firstValue.onSuccess(mListSession)
+    }
+
+    private fun configureListSessionError() {
+        listSessionsPresenter.onLoad()
+        verify(mockLisSessions).execute(captor.capture(), isNull())
+        captor.firstValue.onError(Exception(""))
+    }
+
+    private fun configureBundle() {
+        whenever(mockIBundleFactory.create()).thenReturn(BundleWrapper())
+        whenever(mockView.name).thenReturn("ViewName")
+        whenever(mockIRouter.getNext(any(),any())).thenReturn(validRoute)
     }
 
 
