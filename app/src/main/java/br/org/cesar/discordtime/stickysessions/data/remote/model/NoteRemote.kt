2a4b2bd3817746178170f17e4f63417ae878534f
Omni <engomneya.osman@gmail.com>
2019-10-23 01:01:19 +0200

    add @JvmField

--- app/src/main/java/br/org/cesar/discordtime/stickysessions/data/remote/model/NoteRemote.kt
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/data/remote/model/NoteRemote.kt
@@ -4,21 +4,21 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 
-data class NoteRemote (
+data class NoteRemote(
 
-    @SerializedName("id")
-    @Expose
-    var id: String? = null,
-    @SerializedName("description")
-    @Expose
-    var description: String? = null,
-    @SerializedName("user")
-    @Expose(serialize = false)
-    var user: String? = null,
-    @SerializedName("topic")
-    @Expose
-    var topic: String? = null,
-    @SerializedName("session_id")
-    @Expose
-    var sessionId: String? = null
+        @SerializedName("id")
+        @Expose
+        @JvmField var id: String? = null,
+        @SerializedName("description")
+        @Expose
+        @JvmField var description: String? = null,
+        @SerializedName("user")
+        @Expose(serialize = false)
+        @JvmField var user: String? = null,
+        @SerializedName("topic")
+        @Expose
+        @JvmField var topic: String? = null,
+        @SerializedName("session_id")
+        @Expose
+        @JvmField var sessionId: String? = null
 )
