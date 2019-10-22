483f76b41e036a9b516f4e4108f2cf929ae1e094
Omni <engomneya.osman@gmail.com>
2019-10-23 00:14:51 +0200

    convert ViewNames from java to kotlin

---
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/ui/ViewNames.kt
@@ -0,0 +1,11 @@
+package br.org.cesar.discordtime.stickysessions.ui
+
+object ViewNames {
+
+    const val LOGIN_ACTIVITY = "login_activity"
+    const val LOBBY_ACTIVITY = "lobby_activity"
+    const val MEETING_ACTIVITY = "meeting_activity"
+    const val SESSION_ACTIVITY = "session_activity"
+    const val LIST_ACTIVITY = "list_activity"
+
+}
