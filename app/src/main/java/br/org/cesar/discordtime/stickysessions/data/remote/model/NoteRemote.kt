29cea7ceb31e97f168bf60c220eafc4881ae3ee9
Omni <engomneya.osman@gmail.com>
2019-10-25 22:24:39 +0200

    [#147]Convert to Kotlin- data/remote/model/NoteRemote.java
    1-Add new data kotlin class NoteRemote in data/remote/model package with fields provided in NoteRemote.java but in kotlin .
    2-remove NoteRemote.java

--- app/src/main/java/br/org/cesar/discordtime/stickysessions/data/remote/model/NoteRemote.kt
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/data/remote/model/NoteRemote.kt
@@ -3,22 +3,15 @@ package br.org.cesar.discordtime.stickysessions.data.remote.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-
 data class NoteRemote(
-
-        @SerializedName("id")
-        @Expose
-        @JvmField var id: String? = null,
-        @SerializedName("description")
-        @Expose
-        @JvmField var description: String? = null,
-        @SerializedName("user")
-        @Expose(serialize = false)
-        @JvmField var user: String? = null,
-        @SerializedName("topic")
-        @Expose
-        @JvmField var topic: String? = null,
-        @SerializedName("session_id")
-        @Expose
-        @JvmField var sessionId: String? = null
+        @Expose @SerializedName("id")
+        var id: String? = "",
+        @Expose @SerializedName("description")
+        var description: String? = "",
+        @Expose(serialize = false) @SerializedName("user")
+        var user: String? = "",
+        @Expose @SerializedName("topic")
+        var topic: String? = "",
+        @Expose @SerializedName("session_id")
+        var sessionId: String? = ""
 )
