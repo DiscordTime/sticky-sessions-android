0f86f2b1bf84c58601e31ecdad04687bf21629d3
Patrick Steiger <psteiger@motorola.com>
2019-10-30 16:47:19 -0300

    Converting SessionComponent to Kotlin

---
+++ app/src/main/java/br/org/cesar/discordtime/stickysessions/injectors/components/SessionComponent.kt
@@ -0,0 +1,22 @@
+package br.org.cesar.discordtime.stickysessions.injectors.components
+
+import br.org.cesar.discordtime.stickysessions.injectors.modules.AuthModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.ContextModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.HttpLoggingInterceptorModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.HttpModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.HttpNetworkInterceptorModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.LoggerModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.NetworkModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.NoteModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.ServerModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.SessionModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.SessionPresenterModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.ThreadModule
+import br.org.cesar.discordtime.stickysessions.injectors.modules.UserModule
+import br.org.cesar.discordtime.stickysessions.ui.session.SessionActivity
+import dagger.Component
+
+@Component(modules = [SessionPresenterModule::class, NoteModule::class, SessionModule::class, ThreadModule::class, ContextModule::class, ServerModule::class, UserModule::class, LoggerModule::class, NetworkModule::class, HttpLoggingInterceptorModule::class, HttpNetworkInterceptorModule::class, HttpModule::class, AuthModule::class])
+interface SessionComponent {
+    fun inject(activity: SessionActivity)
+}
