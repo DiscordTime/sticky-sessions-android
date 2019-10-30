688e128e290918013d1006ec8084157ba0016e0e
Patrick Steiger <psteiger@motorola.com>
2019-10-30 16:34:24 -0300

    Converting Mapper - Java to Kotlin

--- app/src/main/java/br/org/cesar/discordtime/stickysessions/data/repository/mapper/Mapper.java
+++
@@ -1,7 +0,0 @@
-package br.org.cesar.discordtime.stickysessions.data.repository.mapper;
-
-public interface Mapper<A, B> {
-
-    public B mapFromDomain(A domainType);
-    public A mapToDomain(B dataType);
-}
